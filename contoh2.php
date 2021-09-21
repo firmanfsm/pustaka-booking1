ac0bb2e24777aa6d4f1b2ce697ab4ed2a6ebcf52
firmanfsm <firmansyahmulyanto08@gmail.com>
2021-09-21 21:02:53 +0700

    contoh2.php

---
+++ contoh2.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh2 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya firmansyah, hobi saya sepakbola";
+    }
+    
+    
+    
+
+}
